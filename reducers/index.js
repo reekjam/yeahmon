928379cc62120ac15e8e7a376b20aad0b1298bdc
Rick Rieta <rick@vermonster.com>
2017-03-16 18:02:26 -0400

    Add redux and scaffold actions and reducers

---
+++ reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  playerHand: '',
+  cpuHand: '',
+  playerScore: '',
+  cpuScore: ''
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
