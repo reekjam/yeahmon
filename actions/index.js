5aa9dc2583cf357cea5ad9f123cc89113fc5be18
Rick Rieta <rick@vermonster.com>
2017-03-31 17:10:50 -0400

    Set up thunk'd actions

--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,75 @@
-import { START_MATCH } from '../constants';
+import {
+  handOptions,
+  START_MATCH,
+  END_MATCH,
+  PLAYER_WIN,
+  PLAYER_LOSE,
+  TIE_GAME,
+  DESCRIBE_PLAYER_WIN,
+  DESCRIBE_PLAYER_LOSE,
+  DESCRIBE_TIE_GAME
+} from '../constants';
+
+const randomHand = () => {
+  return handOptions[Math.floor(Math.random() * handOptions.length)]
+}
 
 export const startMatch = (playerHand) => {
-  return {
-    type: START_MATCH,
-    playerHand
+  return dispatch => {
+    const cpuHand = randomHand();
+
+    if (playerHand === cpuHand) {
+      dispatch(tieGame(playerHand))
+    };
+
+    const playerIndex = handOptions.indexOf(playerHand);
+
+    if (handOptions[(playerIndex + 1) % handOptions.length] == cpuHand) {
+      dispatch(playerLose(playerHand))
+    };
+
+    if (handOptions[(playerIndex + 2) % handOptions.length] == cpuHand) {
+      dispatch(playerWin(playerHand))
+    };
+
+    dispatch(endMatch(playerHand, cpuHand));
   };
 };
+
+const tieGame = () => {
+  return {
+    type: TIE_GAME,
+    results: {
+      verdict: 'tie game',
+      descriptor: DESCRIBE_TIE_GAME
+    }
+  }
+}
+
+const playerWin = () => {
+  return {
+    type: PLAYER_WIN,
+    results: {
+      verdict: 'you win',
+      descriptor: DESCRIBE_PLAYER_WIN
+    }
+  }
+}
+
+const playerLose = (store) => {
+  return {
+    type: PLAYER_LOSE,
+    results: {
+      verdict: 'you lose',
+      descriptor: DESCRIBE_PLAYER_LOSE
+    }
+  }
+}
+
+const endMatch = (playerHand, cpuHand) => {
+  return {
+    type: END_MATCH,
+    playerHand: playerHand,
+    cpuHand: cpuHand
+  }
+}
